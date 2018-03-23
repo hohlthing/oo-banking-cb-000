102a47498f4dd90d1688a8cf4fdb7e492756fcd6
michael hohl <mhohl@rocketmail.com>
2018-03-23 20:03:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,13 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @status == 'pending' && @sender.balance < @amount
+    if !self.valid? && @sender.balance < @amount
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    elsif @status == 'pending'
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = 'complete'
-    else
-      @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
     end
   end
 

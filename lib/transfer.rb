fba988b798a860a380b84d22cfb7934eb936e84e
michael hohl <mhohl@rocketmail.com>
2018-03-23 19:53:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,7 +9,18 @@ class Transfer
   end
 
   def valid?
-    @sender.valid? && @sender.balance - @amount >= 0 && @receiver.valid?
+    @sender.valid? && @sender.balance < @amount && @receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = 'complete'
+    else
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
 end

551469a99ab0057a524258d6061fa7c795ffc050
michael hohl <mhohl@rocketmail.com>
2018-03-23 19:44:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,14 +1,15 @@
 class Transfer
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
+    @status = 'pending'
   end
 
   def valid?
     @sender.BankAccount.valid && @receiver.BankAccount.valid
   end
-  
+
 end

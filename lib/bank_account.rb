93b743a006a281d0313380422f2bd6b3b7218374
michael hohl <mhohl@rocketmail.com>
2018-03-23 19:38:01 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,18 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = 'open'
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
 end
